10ee8c8631e66f6e55b8053a754e2c36cc5ddf4a
truong.hd154023 <truong.hd154023@sis.hust.edu.vn>
2021-06-21 16:56:22 +0700

    update CORS

--- BlazorWasm_in_MVC/Startup.cs
+++ BlazorWasm_in_MVC/Startup.cs
@@ -17,13 +17,24 @@ namespace BlazorWasm_in_MVC
         {
             Configuration = configuration;
         }
-
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.AllowAnyOrigin();
+                                  });
+            });
+
             services.AddControllersWithViews();
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +54,8 @@ namespace BlazorWasm_in_MVC
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseAuthorization();
 
